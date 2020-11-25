c8f6ad86b9582c94761cade2d37b52d2df27b568
issamBoutissante <boutissante.issam.dev@gmail.com>
2020-11-25 18:48:52 +0100

    finally

---
+++ public/electron.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+
+function createWindow() {
+  Menu.setApplicationMenu(null);
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
